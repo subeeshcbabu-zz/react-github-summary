c8072c0e1cb3ac24cbadd1a4b085b2f01b095d38
subeesh chothendavida <subeeshcbabu@users.noreply.github.com>
2016-01-25 22:07:51 -0800

    Update githubsummary.jsx

--- githubsummary.jsx
+++ githubsummary.jsx
@@ -1,12 +1,16 @@
 const GITHUB_API = 'https://api.github.com/users/';
 var GitHubData = React.createClass({
-  state: {},
-  componentDidMount: () => {
-    //Ajax call using JQuery
-    $.get(`GITHUB_API{this.props.username}`, data => this.setState(data));
+  getInitialState() {
+    return {};
   },
-  render: () =>
-    (
+  componentDidMount() {
+    //Ajax call using JQuery
+    $.get(`${GITHUB_API}${this.props.username}`, data => {
+      this.setState(data);
+    });
+  },
+  render() {
+    return (
       <div>
         <img id='avatar' src={this.state.avatar_url} alt={this.state.name} width='100'/>
         <div id='name'>{this.state.name}</div>
